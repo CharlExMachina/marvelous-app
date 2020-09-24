5e4b6bb9ecabd8d1659025031f8f051933650941
Carlos Eduardo Pérez <carlospe097@outlook.com>
2020-09-24 00:55:42 -0600

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { CharacterBio } from './components/CharacterBio';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { NavBar } from './components/NavBar';
+import { HomePage } from "./components/HomePage";
+import { HeroGallery } from './components/HeroGallery';
+import { HeroBio } from './components/HeroBio';
 
 function App() {
   return (
-    <div className="border p-3">
-      <CharacterBio />
+    <div>
+      <Router>
+      <NavBar />
+      <div style={{marginTop: 80}}>
+      <Switch>
+          <Route path="/characters" exact component={HeroGallery} />
+          <Route path="/characters/:id" component={HeroBio} />
+          <Route path="/" component={HomePage} />
+      </Switch>
+      </div>
+      </Router>
     </div>
   );
 }
