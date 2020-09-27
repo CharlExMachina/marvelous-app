72122f7c140ca0ab0333021c1362ffffeec228b8
Carlos Eduardo Pérez <carlospe097@outlook.com>
2020-09-26 23:02:49 -0600

    Converted Axios requests into a hook

--- src/components/HeroGallery.js
+++ src/components/HeroGallery.js
@@ -1,47 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import useAxiosRequest from '../hooks/useAxiosRequest';
 import { HeroCard } from './HeroCard';
+import { LoadingIndicator } from './LoadingIndicator';
 
 export const HeroGallery = () => {
-    const [data, setData] = useState(null);
-    const [currentPage, setCurrentPage] = useState(0);
-    const [isLoading, setIsLoading] = useState(true);
+    const serverResponse = useAxiosRequest("characters", {
+        "limit": 20
+    });
 
-    const publicKey = "badd148cc7d5975dc382dfabd381d794";
-    const privateKey = "b4d93fa4acce25dda350639fd30a1aa173f34bd7";
-    const baseUrl = "https://gateway.marvel.com/v1/public/";
+    let content = null;
 
-    useEffect(() => {
-        const axiosInstance = axios.create({
-        baseURL: baseUrl,
-        params: {
-            "apikey": publicKey,
-            "limit": 20
-        },
-            timeout: 10000,
-            method: 'get',
-            responseType: 'json',
-        });
-        axiosInstance.get("characters")
-        .then(response => {
-        setData(response.data.data);
-        setIsLoading(false);
-        console.log(response.data.data);
-        })
-        .catch(error => console.log(error));
+    if (serverResponse.loading) {
+        content = <LoadingIndicator />
+    } else if (serverResponse.error) {
+        content = <h1>An error has occured!</h1>
+    } else {
+        content = serverResponse.data && serverResponse.data.results.map(characterInfo =>
+            <HeroCard key={characterInfo.id} info={characterInfo} /> 
+        );
+    }
 
-    }, [currentPage]);
-
-    return (
-        <>
-            {isLoading && <h1>Loading...</h1>}
-            <div>
-            {
-                data && data.results.map(characterInfo =>
-                    <HeroCard key={characterInfo.id} info={characterInfo} /> 
-                )
-            }
-            </div>
-        </>
-    )
+    return content;
 }
